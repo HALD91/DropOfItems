6bfcf6b1712f043ab098ffb5148b99b1b8ffaea4
Sune <s.lundegaard28@gmail.com>
2024-04-04 20:58:16 +0200

    Specific items on player death event removed Specific items on player drop event removed

--- src/main/java/me/HALD91/Drop/Listener/DiamondTools.java
+++ src/main/java/me/HALD91/Drop/Listener/DiamondTools.java
@@ -1,9 +1,14 @@
 package me.HALD91.Drop.Listener;
 
+import me.HALD91.Drop.Drop;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
@@ -11,7 +16,7 @@ import java.util.List;
 public class DiamondTools implements Listener {
     @EventHandler
     public void onDeath(final PlayerDeathEvent e) {
-        List <ItemStack> list = e.getDrops();
+        List<ItemStack> list = e.getDrops();
         list.removeIf(item -> item.isSimilar(new ItemStack(Material.DIAMOND_SWORD)));
         list.removeIf(item -> item.isSimilar(new ItemStack(Material.DIAMOND_HELMET)));
         list.removeIf(item -> item.isSimilar(new ItemStack(Material.DIAMOND_CHESTPLATE)));
@@ -19,5 +24,31 @@ public class DiamondTools implements Listener {
         list.removeIf(item -> item.isSimilar(new ItemStack(Material.DIAMOND_BOOTS)));
         list.removeIf(item -> item.isSimilar(new ItemStack(Material.BOW)));
         list.removeIf(item -> item.isSimilar(new ItemStack(Material.ARROW)));
+        list.removeIf(item -> (item.isSimilar(new ItemStack(Material.GOLDEN_APPLE)) && item.getDurability() == 0));
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent event) {
+        Item item = event.getItemDrop();
+        ItemStack itemStack = item.getItemStack();
+        Material itemType = itemStack.getType();
+
+        // Check if the dropped item is a specific type OR it is enchanted
+        if ((itemType == Material.DIAMOND_SWORD ||
+                itemType == Material.DIAMOND_HELMET ||
+                itemType == Material.DIAMOND_CHESTPLATE ||
+                itemType == Material.DIAMOND_LEGGINGS ||
+                itemType == Material.DIAMOND_BOOTS ||
+                itemType == Material.BOW ||
+                itemType == Material.ARROW ||
+                (itemType == Material.GOLDEN_APPLE && itemStack.getDurability() == 0)) &&
+                itemStack.getEnchantments().isEmpty()){
+            event.getPlayer().sendMessage("True");
+            //event.setCancelled(true);
+            item.remove();
+        } else {
+            event.getPlayer().sendMessage("False");
+            event.getPlayer().sendMessage(String.valueOf(itemType));
+        }
     }
 }
